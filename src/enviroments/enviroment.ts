06176d59a4f60e59b4c882dd0c756fc35d68361c
Adrian Zavaleta <zavcorp23@gmail.com>
2026-04-18 01:04:12 -0600

    Correcciones perfil administrador

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://uwcarjkxsgnqfnhlldua.supabase.co',
-  supabaseAnonKey: 'sb_publishable_HdjgqjpFI0n4KEgPskgD6A__wioOdVs',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3Y2Fyamt4c2ducWZuaGxsZHVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NTM5NjAsImV4cCI6MjA5MTQyOTk2MH0.tHudG9zW9L3uOxwHwNuHOeXM--4zrRLD9CMN_bSs8cU',
 };
